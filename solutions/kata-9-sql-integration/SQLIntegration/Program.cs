87f48f048cb95d33e165e2f3fba99487bc26c4d3
Kieran Dwyer <git@kdwyer.co.uk>
2025-10-20 16:32:57 +0100

    Add Kata 9 - SQL Integration with database setup and application connectivity

---
+++ solutions/kata-9-sql-integration/SQLIntegration/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace SQLIntegration;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        string connectionString = "server=localhost;database=Kata9DB;user=sa;password=DevPassword1;trustServerCertificate=true;";
+        using SqlConnection connection = new SqlConnection(connectionString);
+
+        connection.Open();
+
+        string getDepartmentsQuery = "SELECT * FROM Departments";
+        using SqlCommand command = new SqlCommand(getDepartmentsQuery, connection);
+        using SqlDataReader reader = command.ExecuteReader();
+        while (reader.Read())
+        {
+            Console.WriteLine($"Department ID: {reader["DepartmentID"]}, Name: {reader["DepartmentName"]}");
+        }
+
+        connection.Close();
+
+    }
+}
